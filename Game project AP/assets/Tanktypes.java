efc214ef2b8a69b780bc0f0edf0b83e7a2d42052
Devesh21526 <104387075+Devesh21526@users.noreply.github.com>
2022-12-27 18:00:22 +0530

    Add files via upload

---
+++ Game project AP/assets/Tanktypes.java
@@ -0,0 +1,8 @@
+package com.mygdx.game;
+import com.badlogic.gdx.Screen;
+
+//for multiple tanks option
+public class Tanktypes implements Screen {
+    final MyGdxGame game;
+
+}
---
+++ Game project AP/assets/try1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="try1" tilewidth="20" tileheight="20" tilecount="4720" columns="80">
+ <image source="Backgroundtbf.jpg" width="1600" height="1199"/>
+</tileset>
